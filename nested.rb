04c9972dcff429a72a57be4c3759bc7736f9924a
Allison Howard <alliemhow@gmail.com>
2018-09-12 18:19:11 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -97,7 +97,7 @@ def changing_alan
      }
 
 		 alans_new_info = "GUI"
-		 programmer_hash[:alan_kay][:known_for].replace({alans_new_info ==> "GUI"})
+		 programmer_hash[:alan_kay][:known_for].replace("GUI")
 
 		 programmer_hash
 end
